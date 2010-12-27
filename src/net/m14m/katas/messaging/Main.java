596f9fd34645efae51dd747d1015272c45b26208
Moss Collum <moss.collum@gmail.com>
2010-12-27 12:36:03 -0500

    First test passing in the dumbest possible way.

--- src/net/m14m/katas/messaging/Main.java
+++ src/net/m14m/katas/messaging/Main.java
@@ -3,11 +3,11 @@ package net.m14m.katas.messaging;
 import java.io.*;
 
 public class Main {
-    private static Writer network;
+    private static PrintWriter network;
     private static Writer console;
 
     public static void setNetwork(Writer network) {
-        Main.network = network;
+        Main.network = new PrintWriter(network);
     }
 
     public static void setConsole(Writer console) {
@@ -15,5 +15,11 @@ public class Main {
     }
 
     public static void main(String... args) {
+        network.println("connect smtp");
+        network.println("To: joe@example.com");
+        network.println();
+        network.println("Hi there!");
+        network.println();
+        network.println("disconnect");
     }
 }
