d1dff90ccf455cf4ae8620f949e16b8d19b0a708
Moss Collum <moss.collum@gmail.com>
2010-12-27 15:12:06 -0500

    Made Network implement ErrorListener and disable itself if errors are received.

--- src/net/m14m/katas/messaging/Network.java
+++ src/net/m14m/katas/messaging/Network.java
@@ -2,8 +2,9 @@ package net.m14m.katas.messaging;
 
 import java.io.*;
 
-public class Network {
+public class Network implements ErrorListener {
     private PrintWriter connection;
+    private boolean disabled = false;
 
     public Network(Writer connection) {
         this.connection = new PrintWriter(connection);
@@ -11,6 +12,11 @@ public class Network {
 
     public void sendMail(Message message) {
         if (!message.isValid()) return;
+        if (disabled) return;
         message.writeTo(connection);
+    }
+
+    public void error(ErrorMessage message) {
+        disabled = true;
     }
 }
--- test/net/m14m/katas/messaging/NetworkTest.java
+++ test/net/m14m/katas/messaging/NetworkTest.java
@@ -7,8 +7,7 @@ import java.io.StringWriter;
 import static org.junit.Assert.*;
 
 public class NetworkTest {
-    private static final Address VALID_ADDRESS = new Address("anita@example.com");
-    private static final Address INVALID_ADDRESS = new Address("invalid");
+    private static final Address ADDRESS = new Address("anita@example.com");
     private static final Body BODY = new Body("Greetings.");
     private StringWriter output = new StringWriter();
     private Network network;
@@ -18,7 +17,7 @@ public class NetworkTest {
     }
 
     @Test public void sendEmailToSpecifiedAddressWithSpecifiedBody() {
-        network.sendMail(new Message(VALID_ADDRESS, BODY));
+        network.sendMail(new Message(ADDRESS, BODY));
         assertEquals("connect smtp\n" +
                 "To: anita@example.com\n" +
                 "\n" +
@@ -27,8 +26,9 @@ public class NetworkTest {
                 "disconnect\n", output.toString());
     }
 
-    @Test public void shouldNotSendIfAddressIsInvalid() {
-        network.sendMail(new Message(INVALID_ADDRESS, BODY));
+    @Test public void shouldNotSendIfNotifiedOfErrors() {
+        network.error(new ErrorMessage("Something's wrong."));
+        network.sendMail(new Message(ADDRESS, BODY));
         assertEquals("should send nothing", "", output.toString());
     }
 }
