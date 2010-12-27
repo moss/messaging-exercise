c5c02b4f845dfc5979da1bf680c7994f5fd87b14
Moss Collum <moss.collum@gmail.com>
2010-12-27 13:21:52 -0500

    Made the next test pass, but violated some of the object calisthenics rules in doing so.

--- src/net/m14m/katas/messaging/Main.java
+++ src/net/m14m/katas/messaging/Main.java
@@ -1,20 +1,25 @@
 package net.m14m.katas.messaging;
 
-import java.io.Writer;
+import java.io.*;
 
 public class Main {
     private static Network network;
-    private static Writer console;
+    private static PrintWriter console;
 
     public static void setNetwork(Writer network) {
         Main.network = new Network(network);
     }
 
     public static void setConsole(Writer console) {
-        Main.console = console;
+        Main.console = new PrintWriter(console);
     }
 
     public static void main(String... args) {
-        network.sendMail(new Address(args[0]), new Body(args[1]));
+        Address address = new Address(args[0]);
+        Body body = new Body(args[1]);
+        if (!address.isValid()) {
+            console.println("Invalid email address: " + address);
+        }
+        network.sendMail(address, body);
     }
 }
