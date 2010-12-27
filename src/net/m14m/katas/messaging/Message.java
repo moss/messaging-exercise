f69f625340336c0e7fca5b2efe7da89d2fb7ab7b
Moss Collum <moss.collum@gmail.com>
2010-12-27 15:41:08 -0500

    I guess we're gonna need something for them all to live in.

--- src/net/m14m/katas/messaging/Message.java
+++ src/net/m14m/katas/messaging/Message.java
@@ -18,10 +18,22 @@ public class Message {
 
     public void writeTo(PrintWriter connection) {
         connection.println("connect smtp");
-        connection.println("To: " + address);
+        connection.println(new HeaderBlock(address));
         connection.println();
         connection.println(body);
         connection.println();
         connection.println("disconnect");
+    }
+
+    private static class HeaderBlock {
+        private Address address;
+
+        public HeaderBlock(Address address) {
+            this.address = address;
+        }
+
+        @Override public String toString() {
+            return "To: " + address;
+        }
     }
 }
