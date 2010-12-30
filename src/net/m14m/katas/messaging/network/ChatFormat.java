3d33775e94383a7eccad19349f5f7675b3386701
Moss Collum <moss.collum@gmail.com>
2010-12-29 19:47:51 -0500

    No abbrevs.

--- src/net/m14m/katas/messaging/network/ChatFormat.java
+++ src/net/m14m/katas/messaging/network/ChatFormat.java
@@ -4,10 +4,10 @@ import net.m14m.katas.messaging.message.*;
 
 public class ChatFormat implements Format {
     public String format(AddressList addressList, Body body) {
-        return "<" + addressList.format("%s") + ">(" + escapeParens(body) + ")";
+        return "<" + addressList.format("%s") + ">(" + escapeClosingParentheses(body) + ")";
     }
 
-    private String escapeParens(Body body) {
+    private String escapeClosingParentheses(Body body) {
         String text = body.toString();
         return text.replace(")", "\\)");
     }
